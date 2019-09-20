cc50b11fc45256256ba051c75f8844f11ba7df3b
Samuel Dorilas <samueldorilas@outlook.com>
2019-09-20 15:01:11 -0400

    Added spring skeleton

---
+++ gamesgalore/src/main/java/com/revature/gamesgalore/gamesgalore/GamesgaloreApplication.java
@@ -0,0 +1,13 @@
+package com.revature.gamesgalore.gamesgalore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesgaloreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesgaloreApplication.class, args);
+	}
+
+}
